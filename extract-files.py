4d8acc527fb33c037354616f93cbd12f469df008
Arian <arian.kulmer@web.de>
2022-06-22 16:31:14 +0200

    miuicamera-sweet: Bring back face detection libs
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837

--- extract-files.py
+++ extract-files.py
@@ -36,6 +36,10 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('libhidltransport.so'),
     'system/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
         .apktool_patch('patches'),
+    ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
+        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
+        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
+        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
